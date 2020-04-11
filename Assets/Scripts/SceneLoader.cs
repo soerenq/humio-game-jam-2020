853425fc2bbd4f2300ff24a9d864f3bfe2ec81f0
Søren Qvist Jensen <soerenqj@humio.com>
2020-04-11 20:07:03 +0200

    Add console navigation text, bathroom scene

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using Humio;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
 	public void LoadScene(string sceneName)
 	{
+		Console.Instance.AddText($"You enter {sceneName}");
 	   SceneManager.LoadScene(sceneName); // Additive? hm.
 	}
 }
