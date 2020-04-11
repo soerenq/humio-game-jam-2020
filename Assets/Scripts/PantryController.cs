eebf90b0208d525dc4e9e13136a762e86545f634
Martin Høj Sørensen <martinsoerensen@humio.com>
2020-04-12 00:15:45 +0200

    Added the boy and a controller script.

---
+++ Assets/Scripts/PantryController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PantryController : MonoBehaviour
+{
+    private bool _isActive = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            _isActive = false;
+        }
+
+        if(_isActive)
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = mousePos;
+            // if you want to smooth movement then lerp it
+        }
+    }
+    
+    void OnMouseDown()
+    {
+        _isActive = true;
+    }
+}
