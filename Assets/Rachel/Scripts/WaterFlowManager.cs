4a08665fb2a11bbe3276cae4bca672e1c48dbf16
rachel stoklund <rachel.stoklund@gmail.com>
2020-04-11 15:52:08 +0200

    Adding water pipe graphics

---
+++ Assets/Rachel/Scripts/WaterFlowManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterFlowManager : MonoBehaviour
+{
+    
+    // Initialize our scene and other scripts and objects from here
+    private void Awake() {
+        
+    }
+}
