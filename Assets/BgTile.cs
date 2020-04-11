73e8a5c81148545b06b98ca45b4dada2ecaadc69
rachel stoklund <rachel.stoklund@gmail.com>
2020-04-11 19:27:58 +0200

    Dragndrop of pipes

---
+++ Assets/BgTile.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgTile : MonoBehaviour
+{
+   public bool IsPipeBar = false;
+   public int pipeBarID = -1;
+}
--- Assets/Rachel/Scripts/WaterFlowManager.cs
+++ Assets/Rachel/Scripts/WaterFlowManager.cs
@@ -14,6 +14,10 @@ public class WaterFlowManager : MonoBehaviour
 
     private List<Pipe> pipeList;
 
+    public List<GameObject> pipeBarContent;
+
+    public GameObject SelectedPipe = null;
+
     private bool testFilled = false;
 
     private int gridHeight = 5;
@@ -22,6 +26,7 @@ public class WaterFlowManager : MonoBehaviour
     private void Awake() {
         pipeList = new List<Pipe>();
         generateBackground();
+        Camera.main.transform.position = new Vector3((gridWidth / 2) - 0.5f, (gridHeight / 2) - 1.5f, -10);
     }
 
     private void Update () {
@@ -30,6 +35,22 @@ public class WaterFlowManager : MonoBehaviour
                 testFilled = true;
                 StartCoroutine(testFlow());
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hitData = Physics2D.Raycast(new Vector2(worldPosition.x, worldPosition.y), Vector2.zero, 0);
+            if (hitData && hitData.collider.GetComponent<BgTile>() != null) {
+                if (hitData.collider.GetComponent<BgTile>().IsPipeBar) {
+                    SelectedPipe = pipeBarContent[hitData.collider.GetComponent<BgTile>().pipeBarID];
+                }
+                else {
+                    if (SelectedPipe != null)
+                    Instantiate(SelectedPipe, hitData.transform.position, Quaternion.identity);
+                    SelectedPipe = null;
+                }
+            }
+            
         }
     }
 
@@ -40,16 +61,27 @@ public class WaterFlowManager : MonoBehaviour
                 Instantiate(BackgroundTile, new Vector3(x, y, 0), Quaternion.identity);
             }
         }
-        PlacePipes();
+        generatePipeBar();
+    }
+
+    private void generatePipeBar() {
+        for (int i = 0; i < 6; i++) {
+            BgTile temp =  Instantiate(BackgroundTile, new Vector3(i + 0.8f + (i * 0.1f), -1.5f,0), Quaternion.identity).GetComponent<BgTile>();
+            temp.IsPipeBar = true;
+            temp.pipeBarID = i;
+            Instantiate(pipeBarContent[i], new Vector3(i + 0.8f + (i * 0.1f), -1.5f,0), Quaternion.identity);
+        }
     }
 
     private void PlacePipes() {
+    /*  
         pipeList.Add(Instantiate(HorizontalPipe, new Vector3(0,1,0), Quaternion.identity).GetComponent<Pipe>());
         pipeList.Add(Instantiate(RightUpPipe, new Vector3(1,1,0), Quaternion.identity).GetComponent<Pipe>());
         pipeList.Add(Instantiate(RightDownPipe, new Vector3(1,2,0), Quaternion.identity).GetComponent<Pipe>());
         pipeList.Add(Instantiate(LeftDownPipe, new Vector3(0,2,0), Quaternion.identity).GetComponent<Pipe>());
         pipeList.Add(Instantiate(VerticalPipe, new Vector3(0,3,0), Quaternion.identity).GetComponent<Pipe>());
         pipeList.Add(Instantiate(LeftUpPipe, new Vector3(0,4,0), Quaternion.identity).GetComponent<Pipe>());
+    */
     }
 
     private void ToggleWater(bool filled) {
