223320c7bd21e3f98e7c68e5efddfe091fbad14d
Søren Qvist Jensen <soerenqj@humio.com>
2020-04-11 19:28:08 +0200

    Droppable text

--- Assets/Scripts/DroppableProducer.cs
+++ Assets/Scripts/DroppableProducer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Humio
@@ -15,6 +16,9 @@ namespace Humio
                 Debug.Log($"Produced {item.Name}");
                 Inventory.Instance.Add(item);
             }
+
+            Console.Instance.AddText(
+                $"Received: {produces.Aggregate("", (current, next) => current + ", " + next.Name)}");
         }
     }
 }
