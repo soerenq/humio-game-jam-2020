fc8ff5c98d09dd228ca7e900318c7bf64ff27c7b
Erik Søe Sørensen <erik@humio.com>
2020-04-11 18:28:51 +0200

    Basement: Add spider adversary.

---
+++ Assets/Spider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : MonoBehaviour
+{
+    public RectTransform target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rt = (RectTransform) this.transform;
+        var direction = (target.position - rt.position).normalized;
+        rt.Translate(0.8f * direction);
+    }
+}
